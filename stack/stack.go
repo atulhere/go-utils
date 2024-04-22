5c79cf4a3c29befa3f0595c5eb652e05f1af1f82
Atul <atulsolanki30@gmail.com>
2024-04-22 13:44:06 +0530

    Update stack.go

--- stack/stack.go
+++ stack/stack.go
@@ -12,30 +12,30 @@ type Stack struct{
 
 func (s *Stack) Push( item interface{}){
 
-	s.item = append(s.item, item)
+	s.Item = append(s.Item, item)
 
 }
 
 func (s *Stack) Pop() {
 
-	l := len(s.item)
+	l := len(s.Item)
 	if(l >0){
-		s.item = s.item[:l-1]
+		s.Item = s.Item[:l-1]
 	}
 }
 
 func (s *Stack) Top() (interface{}, error) {
 
-	l := len(s.item)
-	if(len(s.item)==0){
+	l := len(s.Item)
+	if(len(s.Item)==0){
 		return nil, errors.New("The Stack is empty")
 	}
-	return s.item[l-1], nil
+	return s.Item[l-1], nil
 }
 
 func (s *Stack) IsEmpty() bool{
 
-	if(len(s.item) == 0){
+	if(len(s.Item) == 0){
 		return true
 	}
 	return false
